17ee3c76282fe19e18fa963089128ad57981f888
Finn Lawrence <finn.lawrence@gmail.com>
2015-07-16 02:14:05 +1200

    Added animation to homepage

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
 $(document).ready(function () {
-
+    $('#contact-form').on('submit', function () {
+        $('#response-modal').openModal();
+        return false;
+    });
+    sizeBags();
 });
 
 $(window).load(function () {
-
+    sizeBags();
+    $counter = 0;
+    $('.fade-bag').each(function() {
+        if ($(this).is(':visible')) {
+            var delay = 300 * $counter;
+            $(this).delay(delay).animate({opacity: 1}, 800);
+            $counter = $counter + 1;
+        }
+    });
 });
+
+$(window).resize(function () {
+    sizeBags();
+});
+
+function sizeBags() {
+    var aspect = $(window).width() / $(window).height();
+
+    if (aspect > 1) {
+        $('.porter-banner').removeClass("portrait");
+    } else {
+        $('.porter-banner').addClass("portrait");
+    }
+}
 
 function getUrlParameter(sParam) {
     var sPageURL = window.location.search.substring(1);
